a7f79f3dc9d910c92b804229d7ac506c331dad69
claudiodaud <claudio.daud1@gmail.com>
2020-08-06 17:04:10 -0400

    insert resources routes for users, roles, permissions. agree middlewares and prefix route group

--- routes/web.php
+++ routes/web.php
@@ -15,8 +15,19 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->middleware('guest');
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group(['prefix' => 'admin'], function () {
+
+	Route::middleware(['auth'])->group(function () {
+
+		Route::get('home', 'HomeController@panel')->name('home');
+		Route::resource('users','UserController');
+		Route::resource('roles','RoleController');
+		Route::resource('permissions','PermissionController');
+		
+	});
+
+});
