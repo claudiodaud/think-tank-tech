1fd9f658c69b8868c1b80b1966c9a8d368e33a6c
claudiodaud <claudio.daud1@gmail.com>
2020-08-08 19:50:42 -0400

    uninstall vue and install react

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,11 +1,12 @@
 @extends('layouts.app')
-
+@section('title','Think Tank Tech')
 @section('content')
+
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+                <div class="card-header">Dashboard</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -14,11 +15,15 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }} <hr>
-                    <router-view></router-view>
+                    You are logged in!
                 </div>
             </div>
         </div>
     </div>
 </div>
+   
+
+    
+                  
+
 @endsection
