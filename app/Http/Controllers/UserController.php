8da604bb15d2a846e4506d6b922e3b55f81fb720
claudiodaud <claudio.daud1@gmail.com>
2020-08-09 19:58:31 -0400

    change usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,7 +16,7 @@ class UserController extends Controller
     public function index()
     {
         $users = User::get();
-        $user = auth()->user();
+        
         return $users;
     }
 
